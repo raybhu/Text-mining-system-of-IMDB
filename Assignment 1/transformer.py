4a7c06d6b923aad7dd607636654c2c1275eff0f9
Raizo <raizo.cn@gmail.com>
2019-03-06 17:20:18 +0800

    update requirements

--- Assignment 1/transformer.py
+++ Assignment 1/transformer.py
@@ -6,15 +6,6 @@ import math
 import pandas as pd
 from pandas import ExcelWriter
 
-if platform.system() == 'Darwin':
-    movieJSONFile = './movies.json'
-elif platform.system() == 'Windows':
-    movieJSONFile = 'movies.json'
-with open(movieJSONFile, 'r') as f:
-    movies = json.load(f)
-    for movie in movies:
-        print(movie['storyline'])
-
 
 def term_freq(word_list):
     word_dict = {}
@@ -24,6 +15,17 @@ def term_freq(word_list):
         else:
             word_dict[w] = 1
     word_num = float(sum(word_dict.values()))
+    print(word_num)
     for w in word_dict.keys():
         word_dict[w] /= word_num
     return word_dict
+
+
+if platform.system() == 'Darwin':
+    movieFilteredJSONFile = './movies_filtered.json'
+elif platform.system() == 'Windows':
+    movieFilteredJSONFile = 'movies_filtered.json'
+with open(movieFilteredJSONFile, 'r') as f:
+    movies = json.load(f)
+    for movie in movies[0:1]:
+        print(term_freq(movie['storyline'].split()))
