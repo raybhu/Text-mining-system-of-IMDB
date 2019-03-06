4a205d4b38179b54e54c3d94ac62b06d03d729f0
Raizo <raizo.cn@gmail.com>
2019-03-07 06:04:00 +0800

    modify filter.py
    add the nltk stop_word data
    optimize the date filter process

--- filter.py
+++ filter.py
@@ -2,6 +2,23 @@ import platform
 import json
 import re
 import shutil
+import nltk
+from nltk.corpus import stopwords
+from nltk.collocations import BigramCollocationFinder
+from nltk.metrics import BigramAssocMeasures
+
+# nltk.download('stopwords')
+
+
+def stopword_filtered(words):
+    filtered_list = []
+    stop_set = set(stopwords.words('english'))
+    # print(stop_set)
+    for w in words:
+        if w not in stop_set:
+            filtered_list.append(w)
+    return filtered_list
+
 
 if platform.system() == 'Darwin':
     movieJSONFile = './movies.json'
@@ -17,16 +34,14 @@ with open(movieFilteredJSONFile, 'w') as f:
     moviesFilteredList = []
     for movie in movies:
         if movie['storyline'] is not None:
+
+            storyLine = movie['storyline'].lower()
+            storyLine = stopword_filtered(storyLine.split())
+            storyLine = ' '.join(storyLine)
             storyLine = re.sub(
-                '[;:.,!?\-/+^\'_$%*()`~\"@#&={}\[\]|\\\\<>]', '', movie['storyline'])
-            storyLine = storyLine.lower()
-            stopwords = ('a', 'an', 'the', 'he', 'she', 'it')
-            tokens = storyLine.split()
-            tokens = [w for w in tokens if w not in stopwords]
-            storyLine = ' '.join(tokens)
+                '[;:.,!?\-/+^\'_$%*()`~\"@#&={}\[\]|\\\\<>]', '', storyLine)
             name = re.sub('\((.*?)\)', '', movie['name'])
-            tokens = name.split()
-            name = ' '.join(tokens)
+            name = name.strip()
             moviesFilteredList.append(
                 {"name": name, "storyline": storyLine, "link": movie['link']})
     f.write(json.dumps(moviesFilteredList))
